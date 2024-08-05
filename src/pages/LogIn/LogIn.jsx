e0820e97061bdfc51c22bec320737611af16324d
Fritz Haber <fritzhaber6@gmail.com>
2024-08-05 14:34:46 +0300

    first commit

---
+++ src/pages/LogIn/LogIn.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PasswordInput from "../../components/Input/PasswordInput";
+import Navbar from "../../components/Navbar/Navbar";
+
+const Login = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={(e = {})}>
+            <h4 className="text-2xl mb-3">Login</h4>
+
+            <input type="text" placeholder="Email" className="input-box" />
+
+            <PasswordInput />
+
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
+
+            <p className="text-sm text-center mt-4">
+              Not registered yet?
+              <Link to="/signUp" className="font-medium text-primary underline">
+                Create an Account
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
+  theme: {
+    extend: {
+      //colors used in the project
+      colors: {
+        primary: "#2b85ff",
+        secondary: "#ef863e",
+      },
+    },
+  },
+  plugins: [],
+};
